d11d11a1201fed35b1d2af40e022fcbabe5df546
Jason Seawall <jseawall@numurus.com>
2025-07-09 16:37:21 -0700

    **

--- scripts/file_pub_vid_app_node.py
+++ scripts/file_pub_vid_app_node.py
@@ -318,14 +318,21 @@ class NepiFilePubVidApp(object):
     self.publish_status()
 
   def resetCb(self,do_updates = True):
-    if do_updates:
+      self.msg_if.pub_warn("Reseting")
+      if self.node_if is not None:
         pass
-    self.initCb
+      if do_updates == True:
+        pass
+      self.initCb(do_updates = do_updates)
+
 
   def factoryResetCb(self,do_updates = True):
-    if do_updates:
+      self.msg_if.pub_warn("Factory Reseting")
+      if self.node_if is not None:
         pass
-    self.initCb
+      if do_updates == True:
+        pass
+      self.initCb(do_updates = do_updates)
 
 
   ###################
